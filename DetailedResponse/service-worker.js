67045d429bb7fc2c42c56c73850a194c2b96bd9b
Tejasva Dhyani <tejasva.dhyani@paytm.com>
2016-10-27 14:42:46 +0530

    added urlshortener

---
+++ DetailedResponse/service-worker.js
@@ -0,0 +1,25 @@
+self.addEventListener('fetch', function (event) {
+  console.log('Handling fetch event for', event.request.url);
+  var requestUrl = new URL(event.request.url);
+  if (requestUrl.pathname === '/urlshortener/v1/url' &&
+    event.request.headers.has('X-Mock-Response')) {
+    var response = {
+      body: {
+        kind: 'urlshortener#url',
+        id: 'http://goo.gl/IKyjuU',
+        longUrl: 'https://tejasvadhyani.github.io/ServiceWorker/DetailedResponse/index.html'
+      },
+      init: {
+        status: 200,
+        statusText: 'OK',
+        headers: {
+          'Content-Type': 'application/json',
+          'X-Mock-Response': 'yes'
+        }
+      }
+    };
+    var mockResponse = new Response(JSON.stringify(response.body), response.init);
+    console.log('Responding with a mock response body:', response.body);
+    event.respondWith(mockResponse);
+  }
+});
