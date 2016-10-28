cf08d0196c33dd9ef713c531e1650c0cc1e71968
Tejasva Dhyani <tejasva.dhyani@paytm.com>
2016-10-28 13:40:15 +0530

    removed fetchHandler

--- Image/service-worker.js
+++ Image/service-worker.js
@@ -2,7 +2,7 @@
 var version = 1;
 var cacheName = 'static-' + version;
 self.addEventListener('install', installHandler);
-self.addEventListener('fetch', fetchHandler);
+// self.addEventListener('fetch', fetchHandler);
 function installHandler(event) {
   event.waitUntil(
     caches.open(cacheName).then(function (cache) {
